ae46b7f05359db869071c4ebec9ebd92550dea8c
Alex Burlacu <alexandruburlacu03042000@gmail.com>
2024-02-07 16:23:00 +0100

    correzione ciclo infinito

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const buttonElement = document.querySelector("#start");
 
 buttonElement.addEventListener("click", function() {
-    for(let i = 0; i = 100; i++) {
+    for(let i = 0; i <= 100; i++) {
         const div = document.createElement("div");
         console.log(this);
     }
