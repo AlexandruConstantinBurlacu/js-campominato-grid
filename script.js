53639e8400686d0f37eb99871da4bc171cf9063d
Alex Burlacu <alexandruburlacu03042000@gmail.com>
2024-02-07 16:20:40 +0100

    creazione div

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 // creazione quadrati
 const buttonElement = document.querySelector("#start");
 
-buttonElement.addEventListener("click", function {
-    
+buttonElement.addEventListener("click", function() {
+    for(let i = 0; i = 100; i++) {
+        const div = document.createElement("div");
+        console.log(this);
+    }
 });
 
-console.log(buttonElement);
+console.log();
